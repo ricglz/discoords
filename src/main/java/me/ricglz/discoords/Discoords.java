7bc8c53a3765eccc43cb06b58107dc64fb21812f
Ricardo J González C <rjgcastillo@hotmail.com>
2021-01-17 14:30:47 -0600

    Add new configuration and add extra warning logs

--- src/main/java/me/ricglz/discoords/Discoords.java
+++ src/main/java/me/ricglz/discoords/Discoords.java
@@ -12,8 +12,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main class for the Discoords plugin where the listeners are declared
+ */
 public final class Discoords extends JavaPlugin {
-    JDABuilder builder;
     JDA jda;
     TextChannel channel;
 
@@ -21,13 +23,21 @@ public final class Discoords extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
         String token = (String) getConfig().get("token");
-        String channelName = (String) getConfig().get("channel");
+        String channelID = (String) getConfig().get("channel-id");
+        String welcomeMessage = (String) getConfig().get("welcome-message");
         try {
             jda = JDABuilder.createDefault(token).build().awaitReady();
-            channel = jda.getTextChannelsByName(channelName, true).get(0);
-            channel.sendMessage("Server is up weebs!").queue();
+            channel = jda.getTextChannelById(channelID);
+            if (channel == null) {
+                getLogger().warning("The channel id is incorrect");
+                return;
+            }
+            channel.sendMessage(welcomeMessage).queue();
         } catch (LoginException | InterruptedException e) {
-            e.printStackTrace();
+            getLogger().warning("There was a problem building the bot. It may be due to the token");
+            if (e instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
@@ -38,7 +48,7 @@ public final class Discoords extends JavaPlugin {
                 sender.sendMessage("The command doesn't accept as many arguments");
                 return false;
             }
-            if (jda == null) {
+            if (jda == null || channel == null) {
                 sender.sendMessage("Discoord is not available");
                 return true;
             }
@@ -48,7 +58,7 @@ public final class Discoords extends JavaPlugin {
                 String locString = String.format("(%d, %d, %d)", (int) loc.getX(), (int) loc.getY(), (int) loc.getZ());
                 String msg = args.length == 0 ? locString : String.format("%s - %s", locString, args[0]);
                 sender.sendMessage(msg);
-                sendMessage(msg, player.getDisplayName());
+                sendLocation(msg, player.getDisplayName());
             } else {
                 sender.sendMessage("You must be a player!");
             }
@@ -57,7 +67,14 @@ public final class Discoords extends JavaPlugin {
         return true;
     }
 
-    private void sendMessage(String sentMessage, String playerName) {
+    /**
+     * Sends a mesage about the current location and the player name to the
+     * designated discord channel
+     * 
+     * @param sentMessage Message that was sent to the player in Minecraft
+     * @param playerName  Name of the player who executed the command
+     */
+    private void sendLocation(String sentMessage, String playerName) {
         String msg = String.format("%s - by %s", sentMessage, playerName);
         channel.sendMessage(msg).queue();
     }
