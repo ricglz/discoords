829d44fe4b27c7b28dd74c70693e3edb54f3658d
Ricardo J González C <rjgcastillo@hotmail.com>
2021-01-24 11:55:45 -0600

    Add tab options for search coords

--- src/main/java/me/ricglz/discoords/commands/CommandSearchCoords.java
+++ src/main/java/me/ricglz/discoords/commands/CommandSearchCoords.java
@@ -3,7 +3,11 @@ package me.ricglz.discoords.commands;
 import me.ricglz.discoords.Coordinates;
 import me.ricglz.discoords.exceptions.InvalidAmountOfArgumentsException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
@@ -17,5 +21,10 @@ public class CommandSearchCoords extends GeneralCommand {
         Location loc = coords.getCoordinates(args[0]);
         String locString = String.format("(%d, %d, %d)", (int) loc.getX(), (int) loc.getY(), (int) loc.getZ());
         sender.sendMessage(String.format("%s - %s", args[0], locString));
+    }
+
+    @Override
+    protected List<String> getTabCompleteOptions(Server server, Player sender, String label, String[] args) {
+        return args.length == 1 ? new ArrayList<>(discoords.getCoordinates().getList()) : new ArrayList<String>();
     }
 }
